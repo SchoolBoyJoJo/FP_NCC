57a1a03633edb7e643af66a4346c4ae5dbce416c
auliaaptrs <144126244+auliaaptrs@users.noreply.github.com>
2024-04-26 10:25:37 +0700

    private message leak issue

--- public/code.js
+++ public/code.js
@@ -1,65 +1,87 @@
 (function(){
 
     const app = document.querySelector(".app");
-    
     const socket = io();
-    
     let uname;
+    let currentRoom = "public";
 
     app.querySelector(".join-screen #join-user").addEventListener("click", function(){
         let username = app.querySelector(".join-screen #username").value;
-        if(username.length == 0){
+        if(username.length === 0){ 
             return;
         }
-        socket.emit("newuser",username);
+        socket.emit("newuser", { username, room: currentRoom }); 
         uname = username;
         app.querySelector(".join-screen").classList.remove("active");
         app.querySelector(".chat-screen").classList.add("active");
-
     });
 
     app.querySelector(".chat-screen #send-message").addEventListener("click", function(){
         let message = app.querySelector(".chat-screen #message-input").value;
-        if(message.length == 0){
+        if(message.length === 0){
             return;
         }
-        renderMessage("my", {
-            username:uname,
-            text:message
-        });
         socket.emit("chat", {
-            username:uname,
-            text:message
+            username: uname,
+            text: message,
+            room: currentRoom 
         });
         app.querySelector(".chat-screen #message-input").value = "";
     });
 
     app.querySelector(".chat-screen #exit-chat").addEventListener("click", function(){
-        socket.emit("exituser", uname);
+        socket.emit("exituser"); 
         window.location.href = window.location.href;
     });
 
+    app.querySelector(".chat-screen #create-private-room").addEventListener("click", function(){
+        const roomName = prompt("Enter the name of the private room:");
+        if(roomName) {
+            socket.emit("createPrivateRoom", roomName);
+        }
+    });
+
+    app.querySelector(".chat-screen #join-private-room").addEventListener("click", function(){
+        socket.emit("getPrivateRooms");
+    });
+
+    app.querySelector(".chat-screen #exit-private-room").addEventListener("click", function(){
+        socket.emit("exitPrivateRoom");
+    });
+
     socket.on("update", function(update){
-        renderMessage("update", update)
+        renderMessage("update", update);
     });
 
-    socket.on("chat", function(message){
-        renderMessage("other", message)
+    socket.on("chat", function(data){
+        const { username, text, room } = data;
+        const sender = (username === uname) ? "You" : username; 
+        renderMessage("other", { username: sender, text, room });
     });
 
-    function renderMessage(type,message){
+    socket.on("privateRoomsList", function(privateRoomsList){
+        const roomsList = privateRoomsList.join("\n");
+        alert("Available Private Rooms:\n" + roomsList);
+        const roomToJoin = prompt("Enter the name of the room you want to join:");
+        if(roomToJoin && privateRoomsList.includes(roomToJoin)) {
+            socket.emit("joinPrivateRoom", roomToJoin);
+        } else {
+            alert("Invalid room name or room is not available.");
+        }
+    });
+
+    socket.on("chat", function(data){
+        const { username, text, room } = data;
+        const sender = (username === uname) ? "You" : username;
+
+        if (room === "public" && currentRoom !== "public") {
+            renderMessage("other", { username: sender, text, room });
+        }
+    });
+
+    function renderMessage(type, message){
         let messageContainer = app.querySelector(".chat-screen .messages");
-        if(type == "my"){
-            let el = document.createElement("div");
-            el.setAttribute("class", "message my-message");
-            el.innerHTML = `
-                <div>
-                    <div class="name">You</div>
-                    <div class="text">${message.text}</div>
-                </div>
-            `;
-            messageContainer.appendChild(el);
-        } else if(type == "other"){
+        if(type === "other"){
             let el = document.createElement("div");
             el.setAttribute("class", "message other-message");
             el.innerHTML = `
@@ -69,13 +91,12 @@
                 </div>
             `;
             messageContainer.appendChild(el);
-        } else if(type == "update"){
+        } else if(type === "update"){
             let el = document.createElement("div");
             el.setAttribute("class", "update");
             el.innerText = message;
             messageContainer.appendChild(el);
         }
-        // scroll chat to end  
         messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
     }
 
