31fe5d2f2eb709629fa70a35f26df225025a92d6
SchoolBoyJoJo <jonathanprb060504@gmail.com>
2024-04-27 13:32:23 +0700

    fix error

--- server.js
+++ server.js
@@ -62,10 +62,10 @@ io.on("connection", function(socket) {
         }
     });
 
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-});
